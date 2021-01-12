1028f479ccacf7975d44ecb23b823d8b88e260ba
semgortz <150101@csgaugustinus.nl>
2021-01-12 11:43:54 +0000

    30,32

--- JS/H1/H1O32.js
+++ JS/H1/H1O32.js
@@ -10,10 +10,11 @@ function setup() {
 
 function draw() {
   for (var rij = 0;rij < 450;rij += 50) {
+    R = random(0,255);
+    G = random(0,255);
+    B = random(0,255);
+    fill(R,G,B);
     for (var kolom = 0;kolom < 450;kolom += 50) {
-      // R = random(0,255);
-      
-      fill(R,G,B);
       rect(kolom,rij,50,50);
     }
   }
